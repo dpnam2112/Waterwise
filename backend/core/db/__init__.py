7618a4d8e1ea8dd9dac1fb9cf1126b349ee307f6
Nam Phuong Do <dpnam2112@gmail.com>
2024-11-27 11:04:57 +0700

    remove old template

--- backend/core/db/__init__.py
+++
@@ -1,9 +0,0 @@
-from .session import Base, session, session_factory
-from .transactional import Transactional
-
-__all__ = [
-    "Base",
-    "session",
-    "Transactional",
-    "session_factory",
-]
